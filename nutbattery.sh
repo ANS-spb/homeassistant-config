3b340d05d9b72906cac063e164140d8aab79b7fa
ANS-spb <mainframe.cyber.system@gmail.com>
2022-06-03 21:25:29 +0300

    03.06.2022 21:25:29

--- nutbattery.sh
+++ nutbattery.sh
@@ -6,9 +6,9 @@ HCIIF=$"hci0"
 BATHEX=$(gatttool -t random --char-read --uuid 0x2a19 -b $NUTMAC 2>/dev/null | awk '{print $4}')
 BATDEC=$((0x$BATHEX))
 if [ "$BATDEC" == "0" ]; then
-       BATDEC=AWAY
+#       BATDEC=AWAY
        exit 0
 fi
-#echo $BATDEC
+echo $BATDEC
 mosquitto_pub -h 127.0.0.1 -u sonoff -P mohChi3u -t nut/battery -m $BATDEC -r
 exit 0
